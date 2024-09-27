d110edbed5434c5963179d412b521a44db00b52b
brcasp <caioarlensp@gmail.com>
2024-09-27 00:21:24 -0300

    Adicionando os terminais de fato e exibindo um pouco melhor a matriz

--- src/App.java
+++ src/App.java
@@ -3,26 +3,31 @@ import java.util.Arrays;
 
 public class App {
         public static void main(String[] args) {
-            ArrayList<String> terminais = new ArrayList<>(Arrays.asList("Terminal de ananindeua", "Terminal de belém", "Terminal de castanhal", "Terminal de Bragança", "Terminal de fortaleza","Terminal de sei la aonde"));
+            //linha 888 do BRT de belém
+            ArrayList<String> terminais = new ArrayList<>(Arrays.asList("Terminal de ananindeua", "Terminal de belém", "Terminal de castanhal", "Terminal de Bragança", "Terminal de Tauá","Terminal de Santa Izabel"));
             
-            Terminal grafoTerminais = new Terminal(6, terminais);
+            Terminal grafoTerminais = new Terminal(terminais.size(), terminais);
             
 
             grafoTerminais.adicionarArestaEntreTerminais("Terminal de ananindeua", "Terminal de belém", 1);
             grafoTerminais.adicionarArestaEntreTerminais("Terminal de castanhal", "Terminal de Bragança", 1);
-            grafoTerminais.adicionarArestaEntreTerminais("Terminal de fortaleza", "Terminal de sei la aonde", 1);
+            grafoTerminais.adicionarArestaEntreTerminais("Terminal de Tauá", "Terminal de Santa Izabel", 1);
             grafoTerminais.adicionarArestaEntreTerminais("Terminal de ananindeua", "Terminal de Bragança", 1);
+            grafoTerminais.adicionarArestaEntreTerminais("Terminal de belém", "Terminal de castanhal", 1);
+            grafoTerminais.adicionarArestaEntreTerminais("Terminal de belém", "Terminal de Bragança", 1);
+            grafoTerminais.adicionarArestaEntreTerminais("Terminal de belém", "Terminal de Tauá", 1);
+            grafoTerminais.adicionarArestaEntreTerminais("Terminal de belém", "Terminal de Santa Izabel", 1);
 
-            grafoTerminais.conexoes("Terminal de Ananindeua");
-            grafoTerminais.conexoes("Terminal de castanhal");
-            grafoTerminais.conexoes("Terminal de fortaleza");
+
+            grafoTerminais.conexoes("Terminal de belém");
+            
 
             int[] graus = grafoTerminais.grau();
             System.out.println("Grau de todos os terminais: " + Arrays.toString(graus));
     
             
-            int grauEspecifico = grafoTerminais.grau(0); 
-            System.out.println("Grau do Terminal de ananindeua: " + grauEspecifico);
+            int grauEspecifico = grafoTerminais.grau(1); 
+            System.out.println("Grau do Terminal de Belém: " + grauEspecifico);
 
             grafoTerminais.exibirMatriz();
         }
